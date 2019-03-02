b0f2f028a236ff4f1304a8506a57f38d81083902
Nguyễn Quốc Cường <quoccuong.nguyen265@hotmail.com>
2019-03-02 16:45:21 +0700

    Add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import MyImage from './roadmap.png';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-  
+
+  let image = new Image();
+  image.src = MyImage
+  element.appendChild(image);
+
   return element;
 }
 
